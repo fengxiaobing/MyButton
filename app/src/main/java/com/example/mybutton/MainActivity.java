9765c8b05c86e54932a609d068c56e60726c193c
fengxiaobing <516668646@qq.com>
2017-12-17 11:38:01 +0800

    增加自定义viewGroup的控件的简单写法

--- app/src/main/java/com/example/mybutton/MainActivity.java
+++ app/src/main/java/com/example/mybutton/MainActivity.java
@@ -20,5 +20,11 @@ public class MainActivity extends Activity {
                 Toast.makeText(MainActivity.this, "测试", Toast.LENGTH_SHORT).show();
             }
         });
+        findViewById(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "已经出来了", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mybutton/MyLayout.java
+++ app/src/main/java/com/example/mybutton/MyLayout.java
@@ -29,7 +29,7 @@ public class MyLayout extends ViewGroup{
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         //mOrientation == VERTICAL
         measureVertical(widthMeasureSpec, heightMeasureSpec);
     }
@@ -57,19 +57,57 @@ public class MyLayout extends ViewGroup{
             final int childWidth = child.getMeasuredWidth();
             height += childHeight;
             width = Math.max(width,childWidth);
+            height = 600;
+            width = 400;
 
         }
         setMeasuredDimension(width,height);
     }
 
+
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+            int count = getChildCount();
+        int tempHeight = 0;
+        int tempWidth = 0;
+        for (int i = 0; i < count; i++) {
+                View child = getChildAt(i);
+            int width = child.getMeasuredWidth();
+            int height = child.getMeasuredHeight();
+            int paddingTop = getTop();
+//            int mLeft = (r - width)/2;
+//            int mTop = (b - paddingTop - height)/2 + tempHeight;
+            tempHeight = height;
+            tempWidth = width;
+            if(i == 0){
+                int mLeft = (r - width)/2;
+                int mTop = (b - paddingTop - height)/2;
+                child.layout(mLeft,mTop,mLeft+width,mTop+height);
+            }else if(i == 3){
+                //如果是第4个，那么以正方形排列
+                int mLeft = (r - width)/2 + tempWidth;
+                int mTop = (b - paddingTop - height)/2 + tempHeight;
+                child.layout(mLeft,mTop,mLeft+width,mTop+height);
+            }else if(i % 2 == 1){
+                //如果是第基数个，那么竖排
+                int mLeft = (r - width)/2;
+                int mTop = (b - paddingTop - height)/2 + tempHeight* ((i+1)/2);
+                child.layout(mLeft,mTop,mLeft+width,mTop+height);
+            }else {
+                //否则就横排
+                int mLeft = (r - width)/2 + tempWidth* (i/2);
+                int mTop = (b - paddingTop - height)/2;
+                child.layout(mLeft,mTop,mLeft+width,mTop+height);
+            }
+
+
+        }
 
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawColor(Color.BLUE);
+//        canvas.drawColor(Color.BLUE);
     }
 }
